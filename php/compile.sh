de69dcb0b404896cf43b2f3e532a33b28e97fac6
XackiGiFF <m.girsanoff@gmail.com>
2023-10-16 02:50:05 +0300

    upd

--- php/compile.sh
+++ php/compile.sh
@@ -554,7 +554,9 @@ function build_gmp {
 	if cant_use_cache "$gmp_dir"; then
 		rm -rf "$gmp_dir"
 		write_download
-		download_from_mirror "gmp-$GMP_VERSION.tar.xz" "gmp" | tar -Jx >> "$DIR/install.log" 2>&1
+		# download_from_mirror "gmp-$GMP_VERSION.tar.xz" "gmp" | tar -Jx >> "$DIR/install.log" 2>&1
+		_download_file https://github.com/pmmp/DependencyMirror/releases/download/mirror/gmp-$GMP_VERSION.tar.xz
+		cat gmp-$GMP_VERSION.tar.xz | tar -Jx >> "$DIR/install.log" 2>&1
 		write_configure
 		cd "$gmp_dir"
 		RANLIB=$RANLIB ./configure --prefix="$INSTALL_DIR" \
